d3a11aab2c5aaedc8b631e7e3a2f0749bb4f3537
mariohood <manhattan199@hotmail.com>
2022-06-09 10:03:07 -0300

    criado pessoas

---
+++ src/main/java/dev/ifrs/pessoas/Admin.java
@@ -0,0 +1,14 @@
+package dev.ifrs.pessoas;
+
+public class Admin extends Pessoa {
+    
+    private boolean admin = true;
+
+    
+    public boolean isAdmin() {
+        return admin;
+    }
+
+
+
+}
---
+++ src/main/java/dev/ifrs/pessoas/Usuario.java
@@ -0,0 +1,16 @@
+package dev.ifrs.pessoas;
+
+public class Usuario {
+
+    private String anuncio;
+
+    public String getAnuncio() {
+        return anuncio;
+    }
+
+    public void setAnuncio(String anuncio) {
+        this.anuncio = anuncio;
+    }
+    
+    
+}
